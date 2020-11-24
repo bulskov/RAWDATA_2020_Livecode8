c715fd8d875593a8a63d8503c23bda9db73bfb4c
Henrik Bulskov <bulskov@ruc.dk>
2020-11-24 09:50:39 +0100

    Add project files.

---
+++ KnockoutExample2/wwwroot/js/main.js
@@ -0,0 +1,13 @@
+﻿
+require.config({
+    baseUrl: "js",
+    paths: {
+        knockout: "lib/knockout/knockout-latest.debug"
+    }
+
+});
+
+
+require(['knockout'], function(ko) {
+    ko.applyBindings({});
+});
